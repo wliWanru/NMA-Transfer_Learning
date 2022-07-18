bb9ff1a0b8bffcbc7b193ee8c15143886f83fd0a
Wanru Li <50128409+wliWanru@users.noreply.github.com>
2022-07-18 12:31:10 +0800

    add: dataloader from h5 file

---
+++ prepare_files/create_dataloader_wl.py
@@ -0,0 +1,71 @@
+# python script
+# -*- coding: utf-8
+"""
+@author: Wanru Li
+@contact: wliwanru@gmail.com
+I'd like to thank https://github.com/namiyousef/multi-task-learning/blob/main/data/data.py
+for their amazing tutorial
+"""
+import os
+
+import h5py
+import pandas as pd
+import PIL
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset
+from config.config import work_dir
+
+os.chdir(work_dir)
+
+
+class CartoonDataset(Dataset):
+    def __init__(self, h5_filename, transform=None, target_transform=None):
+        self.h5_filename = h5_filename
+        self.img_h5_file = self._load_h5_file(self.h5_filename)
+        self.all_labels = self.img_h5_file['labels'][:]
+
+
+    def __len__(self):
+        return len(self.all_labels)
+
+
+    def __getitem__(self, idx):
+        img = self.img_h5_file['img_data'][idx]
+        label = self.img_h5_file['labels'][idx]
+        # sample = {'data': img,
+        #           'label': label,
+        #           'img_idx': idx}
+        return img, label
+
+
+    def _load_h5_file(self, h5_filename):
+        file = h5py.File(h5_filename, 'r')
+        img_data = file['pic_mat']
+        img_labels = file['labels']
+        return dict(file=file, img_data=img_data, labels=img_labels)
+
+
+h5_filename = r'h5_files\train_dataset.h5'
+train_dataset = CartoonDataset(h5_filename)
+
+train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=50, shuffle=True)
+batch_images, batch_labels = next(iter(train_dataloader))
+print('Batch size:', batch_images.shape)
+plt.figure()
+plt.imshow(batch_images[0])
+plt.show()
+
+h5_filename = r'h5_files\test_dataset.h5'
+test_dataset = CartoonDataset(h5_filename)
+
+test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=50, shuffle=True)
+batch_images, batch_labels = next(iter(test_dataloader))
+print('Batch size:', batch_images.shape)
+plt.figure()
+plt.imshow(batch_images[0])
+plt.show()
+
+
+
