5c617765b0b7f01f178538682b70648cb6231fc3
Wanru Li <50128409+wliWanru@users.noreply.github.com>
2022-07-18 17:04:47 +0800

    fix: img dimension, edit: augmentation examples

--- prepare_files/create_dataloader_wl.py
+++ prepare_files/create_dataloader_wl.py
@@ -15,9 +15,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from torch.utils.data import Dataset
 from config.config import work_dir
-
+import torchvision.transforms as transforms
+# import torchvision.transforms
 
 class H5FileDataset(Dataset):
+    # dataloader output: (pic_indices, color_channel, height, width)
+
     def __init__(self, h5_filename, transform=None, target_transform=None):
         self.h5_filename = h5_filename
         self.img_h5_file = self._load_h5_file(self.h5_filename)
@@ -34,6 +37,9 @@ class H5FileDataset(Dataset):
         # sample = {'data': img,
         #           'label': label,
         #           'img_idx': idx}
+        # img = img.transform([2, 0, 1])
+        img = np.transpose(img, [2, 0, 1])
+        img = img.astype(np.double)
         return img, label
 
 
@@ -41,34 +47,47 @@ class H5FileDataset(Dataset):
         file = h5py.File(h5_filename, 'r')
         img_data = file['pic_mat']
         img_labels = file['labels']
+        # img_data.transpose((2, 0, 1))
         return dict(file=file, img_data=img_data, labels=img_labels)
 
 
-train_dataset = H5FileDataset(r'D:\wliwa\Desktop\FERG_DB_256\h5_files\train_dataset.h5')
-train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=50, shuffle=True)
-batch_images, batch_labels = next(iter(train_dataloader))
-print('Batch size:', batch_images.shape)
-plt.figure()
-plt.imshow(batch_images[0])
-plt.show()
+# train_dataset = H5FileDataset(r'D:\wliwa\Desktop\FERG_DB_256\h5_files\train_dataset.h5')
+# train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=50, shuffle=True)
+# batch_images, batch_labels = next(iter(train_dataloader))
+# print('Batch size:', batch_images.shape)
+# plt.figure()
+# plt.imshow(batch_images[0].permute([1, 2, 0]).to(torch.uint8))
+# plt.show()
+#
+#
+# test_dataset = H5FileDataset(r'D:\wliwa\Desktop\FERG_DB_256\h5_files\test_dataset.h5')
+# test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=50, shuffle=True)
+# batch_images, batch_labels = next(iter(test_dataloader))
+# print('Batch size:', batch_images.shape)
+# plt.figure()
+# plt.imshow(batch_images[0].permute([1, 2, 0]).to(torch.uint8))
+# plt.show()
+#
 
-
-test_dataset = H5FileDataset(r'D:\wliwa\Desktop\FERG_DB_256\h5_files\test_dataset.h5')
-test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=50, shuffle=True)
-batch_images, batch_labels = next(iter(test_dataloader))
-print('Batch size:', batch_images.shape)
-plt.figure()
-plt.imshow(batch_images[0])
-plt.show()
-
-
-kdef_test_dataset = H5FileDataset(r'D:\wliwa\Desktop\KDEF\h5_files\test_dataset.h5')
+kdef_test_dataset = H5FileDataset(r'D:\wliwa\Desktop\KDEF\h5_files\kdef_test_dataset.h5')
 test_dataloader = torch.utils.data.DataLoader(kdef_test_dataset, batch_size=50, shuffle=True)
 batch_images, batch_labels = next(iter(test_dataloader))
 print('Batch size:', batch_images.shape)
 plt.figure()
-plt.imshow(batch_images[0])
+plt.imshow(batch_images[0].to(torch.uint8).permute([1, 2, 0]).to(torch.uint8))
 plt.show()
+
+
+# transform_list = [transforms.RandomRotation(10)]
+transform_list = [transforms.RandomRotation(90), transforms.CenterCrop([562, 562])]
+aug = transforms.Compose(transform_list)
+
+# aug = transforms.CenterCrop([64, 64])
+for x, y in test_dataloader:
+    # x = x.permute([0, 3, 1, 2])  # x: (idx, color_channel, height, width)
+    x_aug = aug(x)
+plt.imshow(x_aug[0].permute([1, 2, 0]).to(torch.uint8))
+
 
 
 
